26834cd8d5b3fed1dd31e382363761e3a28e0500
Nicolas Trapp <n.trapp@du.szmf.de>
2023-07-11 21:43:51 +0200

    test

--- keyboards/crkbd/keymaps/nico/rules.mk
+++ keyboards/crkbd/keymaps/nico/rules.mk
@@ -3,13 +3,19 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+#RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 KEY_OVERRIDE_ENABLE = yes
 
-// no mousekeys
 MOUSEKEY_ENABLE = no        # Mouse keys
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = no           # Audio output on port C6
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 
 // to reduce size of firmware
 // https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
