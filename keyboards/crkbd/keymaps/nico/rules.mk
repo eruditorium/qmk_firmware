145d540caa8f12f29c0a0938ad8b6ce014028ec7
Nicolas Trapp <y.t@gmx.de>
2023-07-12 19:09:20 +0200

    small changes

--- keyboards/crkbd/keymaps/nico/rules.mk
+++ keyboards/crkbd/keymaps/nico/rules.mk
@@ -3,7 +3,7 @@
 #   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-#RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 KEY_OVERRIDE_ENABLE = yes
@@ -14,13 +14,14 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 
-RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
 
-// to reduce size of firmware
-// https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
-// Add these flags to your rules.mk file: EXTRAFLAGS += -flto
-// Drashna, on QMK issue 3224, paraphrased
+# to reduce size of firmware
+# https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+# Add these flags to your rules.mk file: EXTRAFLAGS += -flto
+# Drashna, on QMK issue 3224, paraphrased
 EXTRAFLAGS += -flto
 
-COMBO_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+COMBO_ENABLE = no
+EXTRAKEY_ENABLE = yes
