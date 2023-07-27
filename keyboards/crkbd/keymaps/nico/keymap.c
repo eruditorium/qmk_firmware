7ce7ca69f2fbfa3bb3692e163b91a6313dd09cc0
Nicolas Trapp <y.t@gmx.de>
2023-07-27 21:07:19 +0200

    changed functionskey to be compatible with totem

--- keyboards/crkbd/keymaps/nico/keymap.c
+++ keyboards/crkbd/keymaps/nico/keymap.c
@@ -74,17 +74,12 @@ enum custom_keycodes {
 // RIGHT HAND HOME ROW MODS QWERTY ├─────────────────┐
 #define SHT_J RSFT_T(KC_J)
 
-#define ENTALT LALT_T(KC_ENTER)
-
 #define RAISE LT(_RAISE, KC_PGUP)
 #define LOWER LT(_LOWER, KC_PGDN)
 #define ADJUST MO(_ADJUST)
 
-#define SFTAB LSFT_T(KC_TAB)
-#define SFTUND RSFT_T(KC_UNDERSCORE)
-
-#define GUIBSPC LGUI_T(KC_BSPC)
-#define ALTDEL RALT_T(KC_DEL)
+#define GUI_Z LGUI_T(KC_Z)
+#define ALT_X LALT_T(KC_X)
 
 // ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
 // │ K E Y M A P S                                                                                                          │
@@ -92,10 +87,10 @@ enum custom_keycodes {
 // ▝▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▘
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3(
-    KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    SFTAB,    KC_A,    KC_S,    KC_D,    SHT_F,    KC_G,                      KC_H,    SHT_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFTUND,
-                                         GUIBSPC,  RAISE,   ENTALT,  KC_SPC,  LOWER,   ALTDEL
+    KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+    KC_TAB,    KC_A,    KC_S,    KC_D,    SHT_F,    KC_G,                        KC_H,    SHT_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LCTL,   GUI_Z,   ALT_X,   KC_C,    KC_V,     KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UNDERSCORE,
+                                          KC_BSPC,  RAISE,   KC_ENTER,  KC_SPC,  LOWER,   KC_RALT
   ),
 
 /*
