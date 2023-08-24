68cea05f3bd7206a1b488abfdbb3fb77b128986d
Nicolas Trapp <y.t@gmx.de>
2023-08-24 12:53:56 +0200

    added Unicode support and changed some keys

--- keyboards/crkbd/keymaps/nico/keymap.c
+++ keyboards/crkbd/keymaps/nico/keymap.c
@@ -81,19 +81,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // QWERTY
 //  ,-----------------------------------------------------.                    ,-----------------------------------------------------.
-//  |Tab/LGui|   Q    |   W    |   E    |   R    |   T    |                    |   Y    |   U    |   I    |   O    |   P    | Bksp   |
+//  |   ESC  |   Q    |   W    |   E    |   R    |   T    |                    |   Y    |   U    |   I    |   O    |   P    |  Del   |
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  | CtrlSc |   A    |   S    |   D    |   F    |   G    |                    |   H    |   J    |   K    |   L    | ;/Mous | '/Rctl |
+//  |   Tab  |   A    |   S    |   D    | F/Shft |   G    |                    |   H    | J/Shft |   K    |   L    |   ;/:  |  '/"   |
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  | Shift  |   Z    |   X    |   C    |   V    |   B    |                    |   N    |   M    |   ,    |   .    |   /    | SftEnT |
+//  |  Shift | Z/Ctrl | X/Gui  |  C/Alt |   V    |   B    |                    |   N    |   M    | ,/RAlt | ./Gui  | //Ctrl |  Shift |
 //  `--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------'
-//                                      |  LALT  | LOWER  |Spc/FUN |  | Spc/NAV| RAISE  |ACCENTS |
+//                                      |  BSPC  | RAISE  | ENTER  |  |  Space | LOWER  |RAlt/Del|
 //                                      `--------------------------'  `--------------------------'    
   [_QWERTY] = LAYOUT_split_3x6_3(
     KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,      KC_T,                        KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_DEL,
     KC_TAB,    KC_A,    KC_S,    KC_D,   SHT_F,     KC_G,                        KC_H,    SHT_J,   KC_K,     KC_L,    KC_SCLN,  MC_QUOT,
-    KC_LCTL,   CTL_Z,   GUI_X,   ALT_C,  KC_V,      KC_B,                        KC_N,    KC_M,    ALT_COMM, GUI_DOT, CTL_SLSH, KC_ENTER,
-                                          KC_BSPC,  RAISE,   SHT_ENTER,  KC_SPACE,LOWER, ALT_DEL
+    KC_LSFT,   CTL_Z,   GUI_X,   ALT_C,  KC_V,      KC_B,                        KC_N,    KC_M,    ALT_COMM, GUI_DOT, CTL_SLSH, KC_RSFT,
+                                         KC_BSPC,  RAISE,  KC_ENTER,  KC_SPACE,  LOWER,   ALT_DEL
   ),
 
 /*
@@ -103,21 +103,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    │ l o w e r                                       │
    └─────────────────────────────────────────────────┘
 */
-// Functions and keypad
+// Lower
 //  ,-----------------------------------------------------.                    ,-----------------------------------------------------.
-//  |        |   F1   |   F2   |   F2   |   F4   |        |                    |        |   7    |   8    |   9    |        |        |
+//  |        | Mute   |  Home  |   Up   |  PgUp  |  Next  |                    |    /   |   7    |   8    |   9    |   +    | SelWord|
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  |        |   F5   |   F6   |   F7   |   F8   |        |                    |        |   4    |   5    |   6    |   -    |   /    |
+//  |        | VolUp  |  Left  |  Down  | Right  |  Prv   |                    |    *   |   4    |   5    |   6    |   -    |   =    |
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  |        |   F9   |   F10  |   F11  |   F12  |        |                    |        |   1    |   2    |   3    |   +    |   *    |
+//  |        | VolDwn |  End   |        |  PgDwn |  Stop  |                    |    ,   |   1    |   2    |   3    |   .    |        |
 //  `--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------'
-//                                      |        |        |        |  |   =    |   0    |   .    |
+//                                      |        | Adjust |        |  |        |        |   0    |
 //                                      `--------------------------'  `--------------------------'
   [_LOWER] = LAYOUT_split_3x6_3(
-    _______, KC_MUTE, KC_HOME, KC_UP,   KC_PGUP,  KC_MNXT,                   KC_PSLS,   KC_7,   KC_8,   KC_9,   KC_PPLS,  SELWORD,
-    _______, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT,  KC_MPRV,                   KC_PAST,   KC_4,   KC_5,   KC_6,   KC_MINS,  KC_EQL,
-    _______, KC_VOLD, KC_END,  XXXXXXX, KC_PGDN,  KC_MSTP,                   KC_COMM,   KC_1,   KC_2,   KC_3,   KC_DOT,   _______,
-                                        _______,  ADJUST, _______,  _______, _______,   KC_0
+    UC(0x00B0), KC_MUTE, KC_HOME, KC_UP,   KC_PGUP,  KC_MNXT,                   KC_PSLS,   KC_7,   KC_8,   KC_9,   KC_PPLS,  SELWORD,
+    UC(0x2615), KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT,  KC_MPRV,                   KC_PAST,   KC_4,   KC_5,   KC_6,   KC_MINS,  KC_EQL,
+    UC(0x269D), KC_VOLD, KC_END,  XXXXXXX, KC_PGDN,  KC_MSTP,                   KC_COMM,   KC_1,   KC_2,   KC_3,   KC_DOT,   _______,
+                                           _______,  ADJUST, _______,  _______, _______,   KC_0
   ),
 
 /*
@@ -127,21 +127,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    │ r a i s e                                       │
    └─────────────────────────────────────────────────┘
 */
-// Lower
+// Raise
 //  ,-----------------------------------------------------.                    ,-----------------------------------------------------.
-//  |   ~    |   !    |   @    |   #    |   $    |   %    |                    |   ^    |   &    |   *    |   (    |   )    |  Del   |
+//  |   ~    |    !   |   @    |   #    |   $    |   {    |                    |    }   |   F7   |   F8   |   F9   |  F12   | SelWord|
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  |        | SC_F1  | SC_F2  | SC_F3  | SC_F4  |        |                    |        |   _    |   +    |   {    |   }    |   |    |
+//  |        |    %   |   ^    |   &    |   *    |   [    |                    |    ]   |   F4   |   F5   |   F6   |  F11   |   \    |
 //  |--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-//  |        | Gaming |        |  Vol-  |  Vol+  |  Mute  |                    |        |        |        |        |        |        |
+//  |        |    -   |   _    |    `   |    ~   |   (    |                    |    )   |   F1   |   F2   |   F3   |  F10   |        |
 //  `--------+--------+--------+--------+--------+--------+--------.  .--------+--------+--------+--------+--------+--------+--------'
-//                                      |        |        |        |  |        |        |        |
+//                                      |        |        |        |  |    _   | Adjust |        |
 //                                      `--------------------------'  `--------------------------'
   [_RAISE] = LAYOUT_split_3x6_3(
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LCBR,                   KC_RCBR, KC_F7,   KC_F8,   KC_F9,   KC_F12,  SELWORD,
     _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC,                   KC_RBRC, KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_BSLS,
     _______, KC_MINS, KC_UNDS, KC_GRV,  KC_TILD, KC_LPRN,                   KC_RPRN, KC_F1,   KC_F2,   KC_F3,   KC_F10,  _______,
-                                        _______, _______, _______, _______, ADJUST,  _______
+                                        _______, _______, _______, KC_UNDS, ADJUST,  _______
   ),
 
 
@@ -163,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                      |        |        |        |  |        |        |        |
 //                                      `--------------------------'  `--------------------------'
   [_ADJUST] = LAYOUT_split_3x6_3(
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,                   KC_NUM,  KC_LCAP, KC_LSCR, XXXXXXX, XXXXXXX, _______,
     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                         _______, _______, _______, _______, _______, _______
--- keyboards/crkbd/keymaps/nico/rules.mk
+++ keyboards/crkbd/keymaps/nico/rules.mk
@@ -11,6 +11,7 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
+UNICODE_ENABLE = yes
 
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = yes
